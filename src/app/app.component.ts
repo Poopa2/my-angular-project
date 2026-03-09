b61257c1cb911bca54f480608b501b025ebc0d62
mobilesom46@gmail.com <mobilesom46@gmail.com>
2026-03-09 14:45:13 +0700

    Section03 Control Flow Statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-   imports: [RouterOutlet, FormsModule],
+    imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  quantity: number = 1;
-  pricePerItem: number = 100;
-
-  get totalPrice(): number {
-    return this.quantity * this.pricePerItem;
-  }
+  num: number = 0;
+  
 }
